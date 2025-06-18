2a4f70cf05ce808d76f2a5461143598c8c10ac1d
Mariana18-hub <marianapaduaferreira@bol.com.br>
2025-06-18 08:42:51 -0300

    Python

---
+++ Programação/Lógica de programação/prog-4.py
@@ -0,0 +1,5 @@
+name = input ("Digite seu nome: ")
+age = input ("Digite a sua idade: ")
+print("Olá, "+ name +"! Você tem "+ age + " anos." ) 
+
+
---
+++ Programação/Lógica de programação/prog-5.py
@@ -0,0 +1,5 @@
+item = input ("Valor do produto: ")
+produtos = input ("Quantidade de produtos: ")
+multiplicacao = int(item) * int(produtos)
+print("O valor do produto é:" +  str(multiplicacao))
+
