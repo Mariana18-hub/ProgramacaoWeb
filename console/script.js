19ccec9dda6086e90e2b9535abe5f1fabfe3a5ed
Mariana18-hub <marianapaduaferreira@bol.com.br>
2025-06-18 08:30:40 -0300

    arquivos sobre o console

---
+++ console/script.js
@@ -0,0 +1,13 @@
+lista = []
+
+let item1 = prompt ('Digite um item de supermercado:');
+lista.push(item1)
+
+let item2 = prompt ('Digite um item de supermercado:');
+lista.push(item2)
+
+let item3 = prompt ('Digite um item de supermercado:');
+lista.push(item3)
+
+console.log('Sua lista de compras: ')
+console.log(compras)
